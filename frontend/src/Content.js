a580ac8d1c3e710e2fd04758864d5369a69b7ac0
Parardha Kumar <parardhakumar@gmail.com>
2021-10-30 12:06:52 +0530

    Create Initial Project Structure

---
+++ frontend/src/Content.js
@@ -0,0 +1,5 @@
+const heading = "Hi! I am Parardha";
+
+const introduction = "I am a Software Engineer and Machine Learning Enthusiast. Currently I work as a Software Developer at Barclays."
+
+export { heading, introduction }
