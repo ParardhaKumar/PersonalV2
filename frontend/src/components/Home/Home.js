6c1f6739bb982bf9d3916b3da4d979b67e0a688f
Parardha Kumar <parardhakumar@gmail.com>
2021-11-04 01:39:38 +0530

    Add FA and IIT Dhanbad to Timeline

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,7 +2,6 @@ import React,{Component} from 'react';
 import Typewriter from 'typewriter-effect';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import { SocialIcon } from 'react-social-icons';
 
 import * as Content from './Content';
 import Timeline from '../Timeline/Timeline';
--- frontend/src/components/Timeline/Timeline.js
+++ frontend/src/components/Timeline/Timeline.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGitAlt, faJava } from '@fortawesome/free-brands-svg-icons'
 
 import './Timeline.css';
 
@@ -21,18 +21,20 @@ class Timeline extends Component{
                             <div class="timeline-content">
                                 <div><img src="./Barclays.png"></img></div>
                                 <p>I am currently working as a Software Developer with the Business Banking Team.</p>
-                                <p>Skills Used: /></p>
+                                <p>Skills Used: <FontAwesomeIcon icon={faGitAlt} />
+                                <FontAwesomeIcon icon={faJava} />
+                                </p>
                             </div>
                         </li>
                         <li class="timeline-item">
                             <div class="timeline-info">
-                                <span>March 23, 2016</span>
+                                <span>May 2019</span>
                             </div>
                             <div class="timeline-marker"></div>
                             <div class="timeline-content">
-                                <h3 class="timeline-title">Event Title</h3>
-                                <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis
-                                    eu pede mollis pretium. Pellentesque ut neque. </p>
+                            <div><img src="./IIT.png"></img></div>
+                                <h3 class="timeline-title">Mathematics & Computing</h3>
+                                <p>Integrated Master of Technology in Mathematics & Computing with Honours (2014-2018) </p>
                             </div>
                         </li>
                         <li class="timeline-item period">
