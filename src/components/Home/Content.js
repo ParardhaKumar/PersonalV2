aca2d3690691182eb34a3c4bd2597def186e230e
Parardha Kumar <parardhakumar@gmail.com>
2022-01-13 03:15:24 +0530

    Update Intro

--- src/components/Home/Content.js
+++ src/components/Home/Content.js
@@ -1,5 +1,8 @@
 const heading = "Hi! I am ";
 const i = "I ";
-const introduction = ["am a Chess & Crypto Enthusiast.", "work as a Software Engineer at Barclays.", " might be found in various Machine Learning & Web Development Discord Servers"]
+const intro1 = "am a Chess & Crypto Enthusiast.";
+const intro2 = "work as a Software Engineer at Barclays.";
+const intro3 = String.fromCharCode(9825) + " to read about Machine Learning, AI & Web Development";
+const introduction = [intro1, intro2, intro3];
 const resume = "https://drive.google.com/file/d/0B8YhMJAaVUJUOUNGWEhMLVNadXc/view?usp=sharing&resourcekey=0-t3i5iFh_4Ny0sGj71eOwQg";
 export { i, heading, introduction, resume }
